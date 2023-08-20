bfecf1dc19aae222d512f6793301cf40486275b7
Thomas Hamelin <thomashamelin27@outlook.fr>
2023-08-20 15:54:10 +0200

    dockerize applications

--- frontend/src/api/project.ts
+++ frontend/src/api/project.ts
@@ -1,8 +1,16 @@
-import { client } from "@api/client";
-import { CreateProjectRequest, Project } from "@type/Project";
+import { client } from '@api/client';
+import { CreateProjectRequest, Project } from '@type/Project';
 
-export const getProjectByUser = (token : string) => client.get<Project[]>('projects', { 
-  Headers: { Authorization: 'Bearer ' + token}}).then(r => r.data);
+export const getProjectByUser = (token: string) =>
+  client
+    .get<Project[]>('projects', {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+    .then((r) => r.data);
 
-export const createProject = (json: CreateProjectRequest, token: string) => client.post<Project>('projects', {
-  Headers: { Authorization: 'Bearer ' + token}}).then(r => r.data);
+export const createProject = (json: CreateProjectRequest, token: string) =>
+  client
+    .post<Project>('projects', {
+      headers: { Authorization: 'Bearer ' + token },
+    })
+    .then((r) => r.data);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
   return defineConfig({
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
     server: {
+      host: true,
+      strictPort: true,
       port: 3000,
     },
-    plugins: [react(), tsconfigPaths(),],
+    plugins: [react(), tsconfigPaths()],
   });
 };
